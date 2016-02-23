196174847c0a331ac483664b74235ad977626499
Stepan Legachev <siberex@gmail.com>
2016-02-24 04:00:48 +0800

    added proper loader for Memcached plugin

---
+++ wp-content/loader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: www.sib.li
+ * Date: 24.02.16
+ * Time: 03:23
+ */
+
+/**
+ * Memcached plugin is replacing WP_Object_Cache class from wp/wp-includes/cache.php
+ * so it should be loaded here.
+ *
+ * @link https://wordpress.org/plugins/memcached/installation/
+ */
+require_once __DIR__ . 'mu-plugins/Memcached-Object-Cache/object-cache.php';
--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -6,8 +6,11 @@
  * Time: 11:11
  */
 
-require_once __DIR__ . '/Memcached-Object-Cache/object-cache.php';
-
+/**
+ * Loading Batcache plugin
+ *
+ * @link https://wordpress.org/plugins/batcache/
+ */
 require_once __DIR__ . '/batcache/advanced-cache.php';
 require_once __DIR__ . '/batcache/batcache.php';
 
