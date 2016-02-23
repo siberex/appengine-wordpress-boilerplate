9637f2a15e8eb08f35b2f529a7df5c652b1dffa4
Stepan Legachev <siberex@gmail.com>
2016-02-23 11:24:40 +0800

    added loader for mu-plugins

---
+++ wp-content/mu-plugins/loader.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: www.sib.li
+ * Date: 23.02.16
+ * Time: 11:11
+ */
+
+require_once __DIR__ . '/Memcached-Object-Cache/object-cache.php';
+
+require_once __DIR__ . '/batcache/advanced-cache.php';
+require_once __DIR__ . '/batcache/batcache.php';
+
