b0417cec9bd3d93e11965c9dbd1d2e0c13504115
Stepan Legachev <siberex@gmail.com>
2016-02-25 10:36:11 +0800

    fixed #1 updated configure script

--- configure.sh
+++ configure.sh
@@ -2,6 +2,7 @@
 
 SCRIPTPATH=$(dirname "$0")
 
+
 # Read options (-p, -i, -d, -v for project id, sql instance name, db name and deployment version)
 while getopts p:i:d:v: opts; do
     case ${opts} in
@@ -56,36 +57,69 @@ fi
 
 echo Configuring PROJECT ${projectId} VERSION ${appVersion}, DBNAME ${dbName} on GCSQL INSTANCE ${sqlInstanceName}
 
+
+getRandomString() {
+    #head -c 33 /dev/random | base64
+    base64 /dev/urandom | tr -d '/+' | dd bs=42 count=1 2>/dev/null
+}
+
+AUTHKEYS=${SCRIPTPATH}/authkeys
+if [ ! -f ${AUTHKEYS} ]; then
+    echo "## Creating authkeys file"
+    sed "s/AUTH_KEY_VALUE/$(getRandomString)/g" ${AUTHKEYS}.template > ${AUTHKEYS}
+    sed -i '' "s/SECURE_AUTHKEY_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    sed -i '' "s/LOGGED_IN_KEY_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    sed -i '' "s/NONCE_KEY_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    sed -i '' "s/AUTH_SALT_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    sed -i '' "s/SECURE_AUTHSALT_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    sed -i '' "s/LOGGED_IN_SALT_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    sed -i '' "s/NONCE_SALT_VALUE/$(getRandomString)/g" ${AUTHKEYS}
+    echo "...done!"
+fi
+
+
+echo "## Creating app.yaml file"
+
+APPYAML=${SCRIPTPATH}/app.yaml
+
 # Replace Project ID and create app.yaml
-sed "s/your-project-id/$projectId/g" ${SCRIPTPATH}/app-default.yaml > ${SCRIPTPATH}/app.yaml
+sed "s/your-project-id/$projectId/g" ${SCRIPTPATH}/app-default.yaml > ${APPYAML}
 
-# Replace Project ID and Cloud SQL instance name and create wp-config.php
-sed "s/your-project-id:wordpress/$projectId:$sqlInstanceName/g" ${SCRIPTPATH}/wp-config-default.php > ${SCRIPTPATH}/wp-config.php
+# Replace App version
+sed -i '' "s/wpfromstarterproject/$appVersion/g" ${APPYAML}
 
-# Replace App version in app.yaml
-sed -i '' "s/wpfromstarterproject/$appVersion/g" ${SCRIPTPATH}/app.yaml
-# Replace DB name in wp-config.php
-sed -i '' "s/wordpress_db/$dbName/g" ${SCRIPTPATH}/wp-config.php
+# Replace Project ID and Cloud SQL instance name
+sed -i '' "s/your-project-id:wordpress/$projectId:$sqlInstanceName/g" ${APPYAML}
 
-echo ...done!
+# Replace DB name
+sed -i '' "s/wordpress_db/$dbName/g" ${APPYAML}
 
+# Replace auth keys placeholder
+PLACEHOLDER='# Replace this line with authkeys file contents'
+#sed -i '' "s/${PLACEHOLDER}/$(cat ${AUTHKEYS})/g" ${APPYAML}
 
-# TODO: Replace AUTH_KEY, SECURE_AUTH_KEY, etc. declarations in wp-config.php with output from
-# openssl rand -base64 42
-# ↑ this should be done only once, not on every configure launch
+sed -i '' "/${PLACEHOLDER}/{
+    r ${AUTHKEYS}
+    d
+}" ${APPYAML}
+
+echo "...done!"
 
 
 # Creating local DB
 #echo "Creating local database (if not exists)..."
 #mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`${dbName}\`"
 
-echo All Done!
+echo "All Done!"
 
 echo "Don’t forget to launch"
-echo "google_sql.py $projectId:$sqlInstanceName"
+echo "# google_sql.py $projectId:$sqlInstanceName"
 echo "to access Cloud SQL instance and create database there:"
-echo "CREATE DATABASE IF NOT EXISTS $dbName;"
+echo "> CREATE DATABASE IF NOT EXISTS $dbName;"
 
 echo "To run configuration again in non-interactive mode, launch"
-echo "sh configure.sh -p $projectId -i $sqlInstanceName -d $dbName -v $appVersion"
+echo "# sh configure.sh -p $projectId -i $sqlInstanceName -d $dbName -v $appVersion"
+
+
+
 
